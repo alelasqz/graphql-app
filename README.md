47d9d0983588a3d0c3e08ecbdcbb8ed87b07037c
Alexander Velasquez <alelasqz@gmail.com>
2018-08-17 11:34:27 -0500

    GraphQL App

---
+++ README.md
@@ -0,0 +1,20 @@
+# GraphQL-App
+App para consumir los datos del proyecto GraphQL
+
+## Instalación
+```
+yarn install
+```
+
+## Uso en Desarrollo
+```
+yarn start
+
+http://localhost:5678/graphql
+```
+
+## Créditos 
+[Alexander Velásquez](http://www.instagram.com/alelasqz)
+
+## Licencia
+[MIT](https://opensource.org/licenses/MIT)
---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+
+class App extends Component {
+  render() {
+    if(this.props.data.loading) return <h1>Cargando...</h1>
+
+    const { cursos } = this.props.data
+    
+    return (
+      <div className="App">
+        <h1>Listado de cursos</h1>
+          {cursos.map(curso => (
+            <div className="Curso">
+              <h3>{ curso.titulo } </h3>
+              <p>{ curso.descripcion }</p>
+              <hr/>
+              <p>Profesor: { curso.profesor.nombre }</p>
+            </div>
+          ))}
+      </div>
+    );
+  }
+}
+
+const CursosQuery = gql`
+  query CursosQuery{
+    cursos{
+      titulo
+      descripcion
+      profesor{
+        nombre
+      }
+    }
+  }
+`
+
+export default graphql(CursosQuery, {
+  options : {
+    pollInterval: 2000
+  }
+})(App);
